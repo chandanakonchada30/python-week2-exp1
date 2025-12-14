663280393d2ed6e6b8d7916686643d65a7d32462
chandanakonchada30 <24331a05e2@mvgrce.edu.in>
2025-12-14 09:52:12 +0530

    Add files via upload

---
+++ week2-1.py
@@ -0,0 +1,7 @@
+#24331A05E2
+#CALCULATION OF SIMPLE INTEREST
+principle=int(input("Enter principle amount: "))
+rate=float(input("Enter rate of interest: "))
+time=int(input("Enter time in years: "))
+simple_interest=(principle*rate*time)/100
+print("Simple Interest is:", simple_interest)   
